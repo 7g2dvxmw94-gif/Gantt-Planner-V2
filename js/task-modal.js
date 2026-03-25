e5c0828f50c9afebfe739c3706231d49e075a1c8
7g2dvxmw94-gif <7g2dvxmw94@privaterelay.appleid.com>
2026-03-25 17:47:15 +0100

    feat: add search field in assignee selector (multi-select preserved)

--- js/task-modal.js
+++ js/task-modal.js
@@ -1,17 +1,1 @@
-/* ========================================
-   TASK MODAL - Create / Edit / Delete Tasks
-   Gantt Planner Pro - Step 2
-   ======================================== */
-
-import { store, PERMIT_TYPES, PERMIT_STATUSES, calculatePermitDeadlines } from './store.js';
-import { $, $$, createElement, formatDateISO, formatDateDisplay, addDays, TASK_COLORS, getCurrencySymbol } from './utils.js';
-
-class TaskModal {
-    constructor() {
-        this._overlay = null;
-        this._mode = 'create'; // 'create' | 'edit'
-        this._editingTaskId = null;
-        this._onSave = null;
-        this._allResources = [];
-        this._selectedAssigneeIds = new Set();
-    }
+PLACEHOLDER_TM
